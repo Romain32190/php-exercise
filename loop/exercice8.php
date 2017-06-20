fda37aded05786bb61be1f5853229793013325af
Romain32190 <romain.lafargue36@gmail.com>
2017-06-20 16:07:31 +0200

    exercice 8

---
+++ loop/exercice8.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <?php
+
+    $i = 0;
+
+    while( $i < 200 )
+    {
+      echo 'Enfin !!!!.' . '<br />';
+      $i+=12;
+    }
+
+
+
+     ?>
+
+  </body>
+</html>
