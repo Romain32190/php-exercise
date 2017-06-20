11df5e8af7ff61ece253cc9bdf69a6e1476a05d3
Romain32190 <romain.lafargue36@gmail.com>
2017-06-20 16:02:30 +0200

    exercice7

---
+++ loop/exercice7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 7</title>
+  </head>
+  <body>
+    <?php
+
+    $i = 1;
+
+    while( $i <= 100 )
+    {
+      echo 'On tient le bon bout.' . '<br />';
+      $i+=15;
+    }
+
+
+
+     ?>
+
+  </body>
+</html>
