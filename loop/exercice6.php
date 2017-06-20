2680c202ed846e6fdcbb11af9590f8196593997c
Romain32190 <romain.lafargue36@gmail.com>
2017-06-20 15:18:47 +0200

    Ajout de l'exercice 6

---
+++ loop/exercice6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+
+    $i = 0;
+
+    while( $i < 20 )
+    {
+      echo 'C\'est presque bon.' . '<br />';
+      $i++;
+    }
+
+
+
+     ?>
+
+  </body>
+</html>
