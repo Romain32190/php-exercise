4d85f200f2fac2b776e20fb97e76d174f060aad1
Romain32190 <romain.lafargue36@gmail.com>
2017-06-20 16:49:18 +0200

    Exercice 1 function

---
+++ function/exercice1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1 fonctions</title>
+  </head>
+  <body>
+    <h1>Hello world</h1>
+
+    <?php
+
+    function test(){
+      return 'true';
+
+    }
+    echo test();
+
+
+     ?>
+
+  </body>
+</html>
