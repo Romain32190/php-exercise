063a6cbaa19dd17d959345cfb68a04d9100eba4e
Romain32190 <romain.lafargue36@gmail.com>
2017-06-21 16:02:02 +0200

    Ajout exercice 8 et correction exercice7

--- function/exercice7.php
+++ function/exercice7.php
@@ -1,21 +1,21 @@
 <?php
 
-function personne($age, $genre){
-  if ($genre === 'homme' && $age < 18){
-    return 'Vous êtes un homme et vous êtes mineur';
+function personne($genre, $age){
+  if ($genre === homme && $age < 18){
+    echo 'Vous êtes un homme et vous êtes mineur';
   }
-  elseif($genre === 'homme' && $age >= 18){
-    return 'Vous êtes un homme et vous êtes majeur';
+  elseif($genre === homme && $age >= 18){
+    echo 'Vous êtes un homme et vous êtes majeur';
   }
-  elseif($genre === 'femme' && $age < 18){
-    return 'Vous êtes une femme et vous êtes mineur';
+  elseif($genre === femme && $age < 18){
+    echo 'Vous êtes une femme et vous êtes mineur';
   }
   else{
-    return 'Vous êtes une femme et vous êtes majeur';
+    echo 'Vous êtes une femme et vous êtes majeur';
   }
 }
 
-echo personne(homme, 16);
+echo personne(homme, 25);
 
 
  ?>
---
+++ function/exercice8.php
@@ -0,0 +1,8 @@
+<?php
+  function nombres($nom1, $nom2, $nom3){
+      return $nom1 + $nom2 + $nom3;
+}
+  echo nombres(5,6,8);
+
+
+?>
