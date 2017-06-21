4a81304f422654a5b24484c539a2a46ba56c4597
Romain32190 <romain.lafargue36@gmail.com>
2017-06-21 17:11:22 +0200

    Exercice8

---
+++ array/exercice8.php
@@ -0,0 +1,12 @@
+<?php
+
+$tableau = array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
+
+
+foreach($tableau as $cle=>$valeur){
+   echo $cle.' : '.$valeur.'<br>';
+
+}
+
+
+ ?>
