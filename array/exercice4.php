0edf8828acd614be0e93a8e8697af3cd8c1dec9d
Romain32190 <romain.lafargue36@gmail.com>
2017-06-21 16:52:43 +0200

    Ajout exercice4

---
+++ array/exercice4.php
@@ -0,0 +1,5 @@
+<?php
+  $tableau = array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
+  $tableau[7] = 'Août';
+  echo $tableau[7];
+?>
