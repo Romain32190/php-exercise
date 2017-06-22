709622edf5ce88bbda77027c9cdb3aaf14ea5d8d
Romain32190 <romain.lafargue36@gmail.com>
2017-06-22 10:15:39 +0200

    Exerice 1 ParamURL

---
+++ ParamURL/Exercice1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exerice 1 param url</title>
+  </head>
+  <body>
+
+    <a href="index.php?nom=Lafargue&amp;prenom=Romain">Bien</a>
+
+  </body>
+</html>
