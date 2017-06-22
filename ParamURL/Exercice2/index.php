68d4beeb92ef69d78a3aee083cedeecc90ea2b99
Romain32190 <romain.lafargue36@gmail.com>
2017-06-22 10:20:48 +0200

    exercice2

---
+++ ParamURL/Exercice2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exerice 1 param url</title>
+  </head>
+  <body>
+
+    <a href="index.php?nom=Lafargue&amp;prenom=Romain&amp;age=20">Yop</a>
+
+  </body>
+</html>
